af43984e67d6dfb51869665a239f8bd0c2cd6702
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-01-11 15:27:53 +0100

    creo i post nel social

--- main.js
+++ main.js
@@ -55,3 +55,46 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+let socialPost = document.querySelector('.posts-list');
+
+function creaPost(author,created,content,media,likes){
+    socialPost.innerHTML +=`
+<div class="post">
+<div class="post__header">
+    <div class="post-meta">                    
+        <div class="post-meta__icon">
+            <img class="profile-pic" src=${author.image} alt=${author.name}>                    
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">${author.name}</div>
+            <div class="post-meta__time">${created}</div>
+        </div>                    
+    </div>
+</div>
+<div class="post__text">${content}</div>
+<div class="post__image">
+    <img src=${media} alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+        </div>
+    </div> 
+</div>            
+</div>
+`
+}//funzione che crea il post social.
+
+for(let i=0; i<posts.length; i++){
+    creaPost(posts[i].author,posts[i].created,posts[i].content,posts[i].media,posts[i].likes);
+}//inietto i vari posts nel social con ciclo for.
+
+
